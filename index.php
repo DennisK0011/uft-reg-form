69fe6daf6d1ad3e06ddf126f5c7d423db42c2a25
Dennis Ketelbuters <dennisketelbuters@MBP-Dennis.local>
2016-07-19 22:35:15 +0200

    init commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<head>
+	<html lang=”en”>
+	<meta charset=”UTF-8”>
+	<link rel=”stylesheet” href=”style.css”>
+	<title>UNITED FIGHTERS TROPHY registration</title>
+</head>
+
+<body>
+<form>
+	<fieldset>
+        <p>
+    	   <label for="firstName">Name</label>
+    	   <input type="text" id="firstName" name="firstName" />
+        </p>
+
+        <p>
+    	   <label for="lastName">Surname</label>
+    	   <input type="text" id="lastName" name="lastName" />
+        </p>
+
+        <P>
+    	   <label for="clubName">club</label>
+    	   <input type="text" id="clubName" name="clubName" />
+        </p>
+
+        <p>
+    	   <label for="mailAdress" >E-mail</label>
+    	   <input type="email" id="mailAdress" name="mailAdress" />
+        </p>
+
+        <p>
+            <input type="submit">
+        </p>
+	</fieldset>
+</form>
+
+</body>
