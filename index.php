fdf5cf1c1595485ec6ddecc8dcb00f00d4cf7791
Dennis Ketelbuters <dennisketelbuters@MBP-Dennis.local>
2016-08-03 22:08:45 +0200

    php validation added

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
 	<meta charset=”UTF-8”>
 	<link rel=”stylesheet” href=”style.css”>
 	<title>UNITED FIGHTERS TROPHY registration</title>
+    
     <?php
-    //include 'config.php';
+    include 'php-validate.php';
     ?>
+
 </head>
 
 <body>
@@ -14,22 +16,26 @@
 	<fieldset>
         <p>
     	   <label for="firstName">Name</label>
-    	   <input type="text" id="firstName" name="firstName" />
+    	   <input type="text" id="firstName" name="firstName" value="<?php echo $fname;?>">
+           <span class="error">* <?php echo $fnameErr;?></span>
         </p>
 
         <p>
     	   <label for="lastName">Surname</label>
-    	   <input type="text" id="lastName" name="lastName" />
+    	   <input type="text" id="lastName" name="lastName" value="<?php echo $lname;?>">
+           <span class="error">* <?php echo $lnameErr;?></span>
         </p>
 
         <P>
     	   <label for="clubName">club</label>
-    	   <input type="text" id="clubName" name="clubName" />
+    	   <input type="text" id="clubName" name="clubName" value="<?php echo $cname;?>">
+           <span class="error">* <?php echo $cnameErr;?></span>
         </p>
 
         <p>
     	   <label for="mailAdress" >E-mail</label>
-    	   <input type="email" id="mailAdress" name="mailAdress" />
+    	   <input type="email" id="mailAdress" name="mailAdress" value="<?php echo $email;?>">
+           <span class="error">* <?php echo $emailErr;?></span>
         </p>
 
         <p>
---
+++ php-validate.php
@@ -0,0 +1,50 @@
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$fname = $lname = $cname = $email = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["firstName"])) {
+    $fnameErr = "Name is required";
+  } else {
+    $fname = test_input($_POST["firstName"]);
+    if (!preg_match("/^[a-zA-Z ]*$/",$fname)) {
+      $fnameErr = "Only letters and white space allowed"; 
+	  }
+	}
+
+if (empty($_POST["lastName"])) {
+    $lnameErr = "Last Name is required";
+  } else {
+    $lname = test_input($_POST["lastName"]);
+    if (!preg_match("/^[a-zA-Z ]*$/",$lname)) {
+      $lnameErr = "Only letters and white space allowed"; 
+	  }
+	}
+
+if (empty($_POST["clubName"])) {
+    $cnameErr = "Club name is required";
+  } else {
+    $cname = test_input($_POST["clubName"]);
+    if (!preg_match("/^[a-zA-Z ]*$/",$cname)) {
+      $cnameErr = "Only letters and white space allowed"; 
+	  }
+	}
+
+  if (empty($_POST["mailAdress"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["mailAdress"]);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+	  }
+	}
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
