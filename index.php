b5df38d732d04b1dcbcd01f511504a53ae3f1fdf
Dennis Ketelbuters <dennisketelbuters@MBP-Dennis.local>
2016-08-27 21:38:31 +0200

    newest

--- index.php
+++ index.php
@@ -1,47 +1,49 @@
+<?php include 'php-validate.php'; ?>
+
 <!DOCTYPE html>
-<head>
-	<html lang=”en”>
-	<meta charset=”UTF-8”>
-	<link rel=”stylesheet” href=”style.css”>
-	<title>UNITED FIGHTERS TROPHY registration</title>
-    
-    <?php
-    include 'php-validate.php';
-    ?>
+<html>
+    <head>
+	   <meta charset=”UTF-8”>
+	   <link rel=”stylesheet” href=”css/style.css”>
+	   <title>UNITED FIGHTERS TROPHY registration</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <form action="send_post.php" method="post">
+        	<fieldset>
+                <p>
+            	   <label for="firstName">Name</label>
+            	   <input type="text" id="firstName" name="firstName" value="<?php echo $fname;?>" required>
+                   <span class="error">* <?php echo $fnameErr;?></span>
+                </p>
 
-</head>
+                <p>
+            	   <label for="lastName">Surname</label>
+            	   <input type="text" id="lastName" name="lastName" value="<?php echo $lname;?>" required>
+                   <span class="error">* <?php echo $lnameErr;?></span>
+                </p>
 
-<body>
-<form action="send_post.php" method="post">
-	<fieldset>
-        <p>
-    	   <label for="firstName">Name</label>
-    	   <input type="text" id="firstName" name="firstName" value="<?php echo $fname;?>">
-           <span class="error">* <?php echo $fnameErr;?></span>
-        </p>
+                <P>
+            	   <label for="clubName">club</label>
+            	   <input type="text" id="clubName" name="clubName" value="<?php echo $cname;?>" required>
+                   <span class="error">* <?php echo $cnameErr;?></span>
+                </p>
 
-        <p>
-    	   <label for="lastName">Surname</label>
-    	   <input type="text" id="lastName" name="lastName" value="<?php echo $lname;?>">
-           <span class="error">* <?php echo $lnameErr;?></span>
-        </p>
+                <p>
+            	   <label for="mailAdress" >E-mail</label>
+            	   <input type="email" id="mailAdress" name="mailAdress" value="<?php echo $email;?>" required>
+                   <span class="error">* <?php echo $emailErr;?></span>
+                </p>
 
-        <P>
-    	   <label for="clubName">club</label>
-    	   <input type="text" id="clubName" name="clubName" value="<?php echo $cname;?>">
-           <span class="error">* <?php echo $cnameErr;?></span>
-        </p>
+                <p>
+                    <input type="submit">
+                </p>
+        	</fieldset>
+        </form>
 
-        <p>
-    	   <label for="mailAdress" >E-mail</label>
-    	   <input type="email" id="mailAdress" name="mailAdress" value="<?php echo $email;?>">
-           <span class="error">* <?php echo $emailErr;?></span>
-        </p>
-
-        <p>
-            <input type="submit">
-        </p>
-	</fieldset>
-</form>
-
-</body>
+        <script src="js/jquery-3.1.0.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        
+        <script src="js/table.js"></script>
+    </body>
+</html>
